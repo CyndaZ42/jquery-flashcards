cf7a2fd0ca01e1fe40d9f67515aa3626c2653bf5
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-02 09:19:41 -0700

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,78 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#walrus-showing").slideToggle();
-    $("#walrus-hidden").slideToggle();
+  $(".jvs-clickable").click(function() {
+    $("#jvs-showing").slideToggle();
+    $("#jvs-hidden").slideToggle();
   });
-  $(".clickable").click(function() {
-    $("#emu-showing").fadeToggle();
-    $("#emu-hidden").fadeToggle();
+  $(".operators-clickable").click(function() {
+    $("#operators-showing").slideToggle();
+    $("#operators-hidden").slideToggle();
   });
-  $(".clickable").click(function() {
-    $("#giraffe-showing").fadeToggle();
-    $("giraffe-hidden").fadeToggle();
+  $(".var-clickable").click(function() {
+    $("#var-showing").slideToggle();
+    $("#var-hidden").slideToggle();
+  });
+  $(".var-name-clickable").click(function() {
+    $("#var-name-showing").slideToggle();
+    $("#var-name-hidden").slideToggle();
+  });
+  $(".funion-clickable").click(function() {
+    $("#funion-showing").slideToggle();
+    $("#funion-hidden").slideToggle();
+  });
+  $(".methods-clickable").click(function() {
+    $("#methods-showing").slideToggle();
+    $("#methods-hidden").slideToggle();
+  });  
+  $(".arguments-clickable").click(function() {
+    $("#arguments-showing").slideToggle();
+    $("#arguments-hidden").slideToggle();
+  });  
+  $(".Parameters-clickable").click(function() {
+    $("#Parameters-showing").slideToggle();
+    $("#Parameters-hidden").slideToggle();
+  });  
+  $(".Return-clickable").click(function() {
+    $("#Return-showing").slideToggle();
+    $("#Return-hidden").slideToggle();
+  });  
+    $(".chaining-methods-clickable").click(function() {
+    $("#chaining-methods-showing").slideToggle();
+    $("#chaining-methods-hidden").slideToggle();
+  });  
+    $(".Strings-clickable").click(function() {
+    $("#Strings-showing").slideToggle();
+    $("#Strings-hidden").slideToggle();
+  });  
+    $(".Booleans-clickable").click(function() {
+    $("#Booleans-showing").slideToggle();
+    $("#Booleans-hidden").slideToggle();
+  }); 
+    $(".Undefined-clickable").click(function() {
+    $("#Undefined-showing").slideToggle();
+    $("#Undefined-hidden").slideToggle();
+  }); 
+    $(".NaN-clickable").click(function() {
+    $("#NaN-showing").slideToggle();
+    $("#NaN-hidden").slideToggle();
+  });
+    $(".Null-clickable").click(function() {
+    $("#Null-showing").slideToggle();
+    $("#Null-hidden").slideToggle();
+  });
+    $(".Alert-clickable").click(function() {
+    $("#Alert-showing").slideToggle();
+    $("#Alert-hidden").slideToggle();
+  });
+    $(".Comments-clickable").click(function() {
+    $("#Comments-showing").slideToggle();
+    $("#Comments-hidden").slideToggle();
+  });
+    $(".jQuery-clickable").click(function() {
+    $("#jQuery-showing").slideToggle();
+    $("#jQuery-hidden").slideToggle();
+  });
+    $(".Attributes-clickable").click(function() {
+    $("#Attributes-showing").slideToggle();
+    $("#Attributes-hidden").slideToggle();
   });
 });
